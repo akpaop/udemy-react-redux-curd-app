a682a4d5487367a27945f468c0149805bb0393c9
Noriyuki <decoy.nori@gmail.com>
2022-02-12 10:28:31 +0900

    App.jsを修正

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 
-				<p>udemy-react-redux-curd-app</p>
+				<p>udemy-react-redux-curd-app start</p>
 			</header>
 		</div>
 	);
