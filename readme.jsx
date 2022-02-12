cc4c47be6904b42801139f31c6587d8b3e416a01
Noriyuki <decoy.nori@gmail.com>
2022-02-12 10:02:44 +0900

    first commit

---
+++ readme.jsx
@@ -0,0 +1,12 @@
+2/12
+// C:\GitHubでVisualStudioCodeを開きnpx create-react-app [プロジェクト名]を実行
+// npx create-react-app udemy-react-redux-curd-app
+// プロジェクトフォルダに移動
+// cd udemy-react-redux-curd-app
+// サーバー実行してブラウザ表示、ホットリロードスタート
+// npm start
+
+//git init
+// git add readme.txt
+
+// git remote add origin https://github.com/akpaop/udemy-react-redux-curd-app.git
