4596d5ec4cb727236774d8da8d71ac39d5b5bd6c
Noriyuki <decoy.nori@gmail.com>
2022-02-12 18:48:51 +0900

    Counter.jsの削除

--- src/compornent/App.js
+++ src/compornent/App.js
@@ -1,54 +1,18 @@
-import PropTypes from 'prop-types';
-import { Counter } from './Counter';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
 
 function App() {
-	const dom = <h1>udemy-react-redux-curd-app start jsx dom</h1>;
-
-	const Users = [
-		{ LastName: 'Taro', FirstName: 'Yamada' },
-		{ LastName: 'Jiro', FirstName: 'Yamada' },
-		{ LastName: 'Saburo', FirstName: 'Yamada' },
-	];
+	const [count, setCount] = useState(1);
 
 	return (
 		<div>
-			{dom}
-			<input
-				type='text'
-				onClick={() => {
-					console.log('Clicked.');
-				}}
-			/>
-
-			{Users.map((User, index) => {
-				return (
-					<DispUser
-						LastName={User.LastName}
-						FirstName={User.FirstName}
-						key={index}
-					/>
-				);
-			})}
-
-			<Counter />
+			<h3>useStete Counter</h3>
+			<p>Counter : {count}</p>
+			<button onClick={() => setCount(count + 1)}>+1</button>
+			<button onClick={() => setCount(count - 1)}>-1</button>
 		</div>
 	);
 }
-
-//FunctionalCompornetは最初大文字縛り
-//ひとまず最初大文字にすればよいのではなかろうか
-const DispUser = (props) => {
-	return (
-		<div>
-			<span>Hello! {props.FirstName} </span>
-			<span>{props.LastName} !!</span>
-		</div>
-	);
-};
-
-DispUser.propTypes = {
-	FirstName: PropTypes.string,
-	LastName: PropTypes.string,
-};
 
 export default App;
