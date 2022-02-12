ca96dff677fe7a0ef12c2625be867bd803ba70d4
Noriyuki <decoy.nori@gmail.com>
2022-02-12 12:07:46 +0900

    props追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 function App() {
 	const dom = <h1>udemy-react-redux-curd-app start jsx dom</h1>;
+
+	const Users = [
+		{ LastName: 'Taro', FirstName: 'Yamada' },
+		{ LastName: 'Jiro', FirstName: 'Yamada' },
+		{ LastName: 'Saburo', FirstName: 'Yamada' },
+	];
+
 	return (
-		<div className='foo'>
+		<div>
 			{dom}
 			<input
 				type='text'
@@ -9,8 +16,29 @@ function App() {
 					console.log('Clicked.');
 				}}
 			/>
+
+			{Users.map((User, index) => {
+				return (
+					<DispUser
+						LastName={User.LastName}
+						FirstName={User.FirstName}
+						key={index}
+					/>
+				);
+			})}
 		</div>
 	);
 }
+
+//FunctionalCompornetは最初大文字縛り
+//ひとまず最初大文字にすればよいのではなかろうか
+const DispUser = (props) => {
+	return (
+		<div>
+			<span>Hello! {props.FirstName} </span>
+			<span>{props.LastName} !!</span>
+		</div>
+	);
+};
 
 export default App;
