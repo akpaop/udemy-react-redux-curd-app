9dc7f3db8b7ebf1dc22c443af78eb0c2e361c2bc
Noriyuki <decoy.nori@gmail.com>
2022-02-13 11:17:29 +0900

    Main.js導入

--- src/compornents/App.js
+++ src/compornents/App.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
+import { Main } from './Main';
 import { SignIn } from './SignIn';
 
 export const App = () => {
 	const [name, setName] = useState('');
 	console.log('App name', name);
-	return (
+
+	const divMain = (
+		<div>
+			udemy-react-app-chat
+			<Main />
+		</div>
+	);
+	const divSignIn = (
 		<div>
 			udemy-react-app-chat
 			<SignIn setName={setName} />
 		</div>
 	);
+
+	if (name !== '') {
+		return divMain;
+	} else {
+		return divSignIn;
+	}
 };
